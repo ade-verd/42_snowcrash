61ec803d0b6b38c4858efac8e6e8d4497b245687
ADV <a.deverdilhac@gmail.com>
2020-09-09 19:19:55 +0200

    [ade-verd] scripts level00

---
+++ level00/resources_adv/script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+LEVEL="00"
+
+export SNOW_USER="level$LEVEL"
+export SNOW_PORT="4242"
+
+if [ -z ${SNOW_HOST+x} ]; then read -p "VM Host: " SNOW_HOST; fi
+if [ -z ${SNOW_PORT+x} ]; then read -p "VM Port (ex: 4242): " SNOW_PORT; fi
+if [ -z ${SNOW_USER+x} ]; then read -p "VM User (ex: level00): " SNOW_USER; fi
+
+CURDIR=`dirname $0`
+FLAG_LEVEL="flag$LEVEL"
+FLAG_CONTENT=`cat $CURDIR/../flag`
+
+# Run solver.sh on level user
+ssh -t -q $SNOW_USER@$SNOW_HOST -p $SNOW_PORT "bash -s" -- < ./solver.sh
+
+# Then check flag on flag user
+ssh -t -q $FLAG_LEVEL@$SNOW_HOST -p $SNOW_PORT "getflag"
+echo -e "\nExpected flag: $FLAG_CONTENT"
