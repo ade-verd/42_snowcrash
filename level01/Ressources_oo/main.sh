69f263c37e73098ebf2e4df6595330a168b481ad
oozkaya <oozkaya@student.42.fr>
2020-09-11 13:58:42 +0200

    [oozkaya] level01 CURDIR_ABS add

--- level01/Ressources_oo/main.sh
+++ level01/Ressources_oo/main.sh
@@ -9,11 +9,12 @@ export SNOW_PORT="4242"
 if [ -z ${SNOW_HOST+x} ]; then read -p "VM Host: " SNOW_HOST; fi
 
 CURDIR=`dirname $0`
+CURDIR_ABS=`dirname "$(readlink -f "$0")"`
 PWD=`pwd`
 SCRIPT="kali_script.sh"
 
 # Create the docker environment and run John the Ripper hacking tool to find flag02 password
-docker run -e SNOW_HOST=$SNOW_HOST -it -v $PWD/$SCRIPT:/$SCRIPT --rm kalilinux/kali-rolling bash kali_script.sh
+docker run -e SNOW_HOST=$SNOW_HOST -it -v $CURDIR_ABS/$SCRIPT:/$SCRIPT --rm kalilinux/kali-rolling bash kali_script.sh
 
 # Then check flag on flag user
 FLAG_LEVEL="flag$LEVEL"
