3834b1ab231f5caa9108c3a3fff55be6e26d43b7
ADV <a.deverdilhac@gmail.com>
2020-09-25 13:12:36 +0200

    [ade-verd] add prev_token, set -x

--- level07/Ressources_adv/main.sh
+++ level07/Ressources_adv/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LEVEL="03"
+LEVEL="07"
 
 CURDIR=`dirname "$(readlink -f "$0")"`
 
@@ -14,7 +14,7 @@ if [ -z ${SNOW_PORT+x} ]; then read -p "VM Port: " SNOW_PORT; fi
 
 FLAG_CONTENT=`cat $CURDIR/../flag`
 
-# Connect to 'level03'
+# Connect to 'level07'
 echo -e "\n$SNOW_USER password is : $PW\n"
 ssh -p $SNOW_PORT $SNOW_USER@$SNOW_HOST 'bash' < $CURDIR/script.sh
 echo -e "\nExpected flag: $FLAG_CONTENT"
