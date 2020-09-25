7000dfb099cc6c9f40cc2361b71767af47d8804c
ADV <a.deverdilhac@gmail.com>
2020-09-25 16:14:59 +0200

    [ade-verd] level09

---
+++ level09/Ressources_adv/main.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+LEVEL="09"
+
+CURDIR=`dirname "$(readlink -f "$0")"`
+
+PREV_LEVEL=`printf "%02d" $((10#$LEVEL-1))`
+PW=`cat $CURDIR/../../level$PREV_LEVEL/flag`
+
+SNOW_USER="level$LEVEL"
+
+if [ -z ${SNOW_HOST+x} ]; then read -p "VM Host: " SNOW_HOST; fi
+if [ -z ${SNOW_PORT+x} ]; then read -p "VM Port: " SNOW_PORT; fi
+
+FLAG_CONTENT=`cat $CURDIR/../flag`
+FLAG="flag$LEVEL"
+
+# Connect to 'level09'
+echo -e "\n\n$SNOW_USER password is : $PW\n"
+ssh -p $SNOW_PORT $SNOW_USER@$SNOW_HOST 'bash' < $CURDIR/script.sh
+
+# Connect to 'flag09' to get the flag
+ssh -q -p $SNOW_PORT $FLAG@$SNOW_HOST getflag
+echo -e "\nExpected flag: $FLAG_CONTENT"
---
+++ level09/Ressources_adv/script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function hexToDec {
+    echo $((16#$1))
+}
+
+function level09Decoder {
+    XXD_OUTPUT=`xxd -c 1 -l 25 $1`
+
+    PASSWORD=''
+
+    echo -e "#\tCODE\tCHAR\t\tCODE\tCHAR"
+    echo "=============================================="
+    while IFS= read -r line; do
+        INDEX_16=`echo $line | cut -d: -f1`
+        INDEX_10=`hexToDec $INDEX_16`
+        echo -en "$INDEX_10\t"
+
+        CHARCODE_16=`echo $line | cut -d ' ' -f2`
+        CHARCODE_10=`hexToDec $CHARCODE_16`
+        echo -en "$CHARCODE_10\t"
+
+        CHAR=`echo $line | cut -d ' ' -f3`
+        echo -en "$CHAR\t"
+
+        DECODED_CHARCODE_10=$(($CHARCODE_10 - $INDEX_10))
+        echo -en ">\t$DECODED_CHARCODE_10\t"
+
+        DECODED_CHAR=`printf "\x$(printf %x $DECODED_CHARCODE_10)"`
+        echo "$DECODED_CHAR"
+
+        PASSWORD+=$DECODED_CHAR
+    done <<< "$XXD_OUTPUT"
+
+    echo -e "\nflag09 Password is: $PASSWORD"
+}
+
+level09Decoder ~/token
